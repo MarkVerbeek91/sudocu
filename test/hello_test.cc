2b38cdf0109d1ffcef30787938b6fb45f247cb7c
Mark Verbeek <mark.verbeek-sneijders@sioux.eu>
2023-07-20 10:56:37 +0200

    test(unittest): added a unit tests

--- test/hello_test.cc
+++ test/hello_test.cc
@@ -6,18 +6,14 @@
 #include "inc/ext/rapidcsv.h"
 #include "test/test_data.h"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  EXPECT_EQ(7 * 6, 42);
-}
-
 TEST(HelloTest, ReadAFile) {
   std::stringstream sstream(sudocu4x4);
 
   rapidcsv::Document doc(sstream, rapidcsv::LabelParams(-1, -1),
-                         rapidcsv::SeparatorParams(','));
+                         rapidcsv::SeparatorParams(' '),
+                         rapidcsv::ConverterParams(true));
 
-  EXPECT_EQ(true, true);
+  std::vector<int> close = doc.GetColumn<int>(0);
+
+  for (int i : close) std::cout << i << std::endl;
 }
